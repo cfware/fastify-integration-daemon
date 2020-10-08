6d0d40735b1571001ee89388703788a542e2ba07
Corey Farrell <git@cfware.com>
2020-10-08 15:02:25 -0400

    feat: Initial implementation

---
+++ README.md
@@ -0,0 +1,6 @@
+# @cfware/fastify-integration-daemon [![NPM Version][npm-image]][npm-url]
+
+Fastify Integration Daemon
+
+[npm-image]: https://img.shields.io/npm/v/@cfware/fastify-integration-daemon.svg
+[npm-url]: https://npmjs.org/package/@cfware/fastify-integration-daemon
---
+++ index.js
@@ -0,0 +1,84 @@
+import fastify from 'fastify';
+import fastifyBabel from 'fastify-babel';
+import fastifyStatic from 'fastify-static';
+import fastifyHttpProxy from 'fastify-http-proxy';
+
+export class FastifyIntegrationDaemon {
+	daemon = fastify();
+
+	serveProxy({prefix, upstream, onResponse}) {
+		const replyOptions = onResponse ? {onResponse} : undefined;
+		this.daemon.register(fastifyHttpProxy, {
+			upstream: `${upstream}${prefix}`,
+			prefix,
+			replyOptions
+		});
+
+		return this;
+	}
+
+	serveBuilt({prefix, root}) {
+		this.daemon.register((fastify, options, next) => {
+			fastify
+				.register(fastifyStatic, {
+					root,
+					redirect: true
+				})
+				.setNotFoundHandler((_, reply) => reply.sendFile('index.html'));
+
+			next();
+		}, {prefix});
+
+		return this;
+	}
+
+	serveSource({prefix, root, manifest, nodeModules, babelrc}) {
+		const babelCache = new Map();
+		this.daemon.get('/purge-cache', (_, reply) => {
+			const message = `Cleared ${babelCache.size} entries`;
+			babelCache.clear();
+			console.log(message);
+			reply.send(`${message}\n`);
+		});
+
+		this.daemon.register((fastify, options, next) => {
+			fastify
+				.register((fastify, options, next) => {
+					fastify.register(fastifyStatic, {
+						root,
+						redirect: true
+					});
+					fastify.get('/manifest.webmanifest', (_, reply) => reply.send(manifest));
+					fastify.setNotFoundHandler((_, reply) => reply.sendFile('index.html'));
+
+					next();
+				}, {prefix})
+				.register(fastifyStatic, {
+					prefix: '/node_modules',
+					root: nodeModules,
+					decorateReply: false
+				})
+				.register(fastifyBabel, {
+					babelrc,
+					maskError: false,
+					cache: babelCache
+				});
+
+			next();
+		});
+
+		return this;
+	}
+
+	listen(...args) {
+		return this.daemon.listen(...args);
+	}
+
+	unref() {
+		this.daemon.server.unref();
+	}
+
+	address() {
+		return this.daemon.server.address();
+	}
+}
